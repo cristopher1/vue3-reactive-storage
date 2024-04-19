f9c157feadca869b9777e7acff2aa5757e3e3c06
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-04-18 21:46:01 -0400

    docs: se cmabia el tipo de reactiveWebStorage

--- src/globals/index.ts
+++ src/globals/index.ts
@@ -1,18 +1,13 @@
 import { ReactiveWebStorage } from '../storage/ReactiveWebStorage.js'
 
-export {}
-
 declare module '@vue/runtime-core' {
   /**
    * When the user installs this plugin and he calls
    * app.config.globalProperties, it will appear this information.
    */
-  export interface ComponentCustomProperties {
-    /**
-     * A function used to obtain a greeting.
-     *
-     * @returns {ReactiveWebStorage} A greeting.
-     */
-    $reactiveWebStorage: () => ReactiveWebStorage
+  interface ComponentCustomProperties {
+    $reactiveWebStorage: ReactiveWebStorage
   }
 }
+
+export {}
