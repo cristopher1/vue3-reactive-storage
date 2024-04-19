a24f2b16fdb660e8523ff53f73ce6336a90e5338
cristopher1 <cristopher.jimene.13@sansano.usm.cl>
2024-04-19 10:39:36 -0400

    test: se agregan tests

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -1,15 +1,5 @@
-import { jest } from '@jest/globals'
 import { faker } from '@faker-js/faker'
-
-const createApp = jest.fn(() => ({
-  config: {
-    globalProperties: {},
-  },
-  install: function (plugin, options = {}) {
-    plugin.install(this, options)
-  },
-}))
 
 faker.seed(17)
 
-export { faker, createApp }
+export { faker }
